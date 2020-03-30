c9590e06d473b013cb679dea98962cbf8ba53eed
pratikKataria <pratikkatariya3.pk@gmail.com>
2020-03-30 19:45:34 +0530

    Navigation Architecture Component implemented

--- app/src/main/java/com/tricky_tweaks/homekeeping/main/MainFragment.java
+++ app/src/main/java/com/tricky_tweaks/homekeeping/main/MainFragment.java
@@ -1,8 +1,9 @@
-package com.tricky_tweaks.homekeeping.main.customer_servces;
+package com.tricky_tweaks.homekeeping.main;
 
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,17 +14,15 @@ import com.tricky_tweaks.homekeeping.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ACServiceAndRepairFragment extends Fragment {
-
-    public ACServiceAndRepairFragment() {
+public class MainFragment extends Fragment {
+    public MainFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ac_service_and_repair, container, false);
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
